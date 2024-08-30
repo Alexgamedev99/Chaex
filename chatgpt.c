b3030dc3868d1569a48c8873f62353ccb79ee65b
Alex <alexgamedev99@gmail.com>
2024-08-30 21:20:31 +0200

    xd

--- chatgpt.c
+++ chatgpt.c
@@ -179,23 +179,23 @@ int * allocateIntArray(int num){
 
 int main(void)
 {
-    int line, col,i,j;
+    int line, col;
     scanf("%d",&col);
     scanf("%d",&line);
     int ** matrix = allocateIntStarArray(line);
-    for(i = 0; i < line; i++){
+    for(int i = 0; i < line; i++){
      matrix[i] = allocateIntArray(col);
     }
 
 
-    for(i = 0; i < line; i++){
-        for(j = 0; j < col; j++){
+    for(int i = 0; i < line; i++){
+        for(int j = 0; j < col; j++){
             scanf("%d",&matrix[i][j]);
         }
     }
 
-     for(i = 0; i < line; i++){
-        for(j = 0; j < col; j++){
+     for(int i = 0; i < line; i++){
+        for(int j = 0; j < col; j++){
             if(matrix[i][j]>1000){
                 printf("[X]");
             }else{
